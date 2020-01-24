eb3047dba3fecee3ede80f41e941a91b04922fab
Maksymilian Kanoza <mkanoza@objectivity.co.uk>
2020-01-24 12:27:49 +0100

    Add project files.

---
+++ Ocaramba.UITests3/PageObjects/InternetPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using NLog;
+using Ocaramba.Extensions;
+using Ocaramba.Types;
+
+namespace Ocaramba.UITests3.PageObjects
+{
+    public class InternetPage : ProjectPageBase
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Locators for elements
+        /// </summary>
+        private readonly ElementLocator
+            linkLocator = new ElementLocator(Locator.CssSelector, "a[href='/{0}']"),
+            basicAuthLink = new ElementLocator(Locator.XPath, "//a[contains(text(),'Auth')]");
+
+        public InternetPage(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        /// <summary>
+        /// Navigate To URL
+        /// </summary>
+        public void OpenHomePage()
+        {
+            var url = BaseConfiguration.GetUrlValue;
+            Logger.Info(CultureInfo.CurrentCulture, "Opening page {0}", url);
+            this.Driver.NavigateTo(new Uri(url));
+        }
+
+        /// <summary>
+        /// Click on Link
+        /// </summary>
+        public void GoToPage(string page)
+        {
+            Logger.Info(CultureInfo.CurrentCulture, "Clicking on element link");
+            var element = this.Driver.GetElement(this.linkLocator.Format(page));
+            element.Click();
+        }
+    }
+}
---
+++ Ocaramba.UITests3/ProjectPageBase.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using Ocaramba;
+
+namespace Ocaramba.UITests3
+{
+    public partial class ProjectPageBase
+    {
+        public ProjectPageBase(DriverContext driverContext)
+        {
+            this.DriverContext = driverContext;
+            this.Driver = driverContext.Driver;
+        }
+
+        protected IWebDriver Driver { get; set; }
+
+        protected DriverContext DriverContext { get; set; }
+    }
+}
